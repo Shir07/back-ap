cd28b01014e9721dcfb17316756a03ac890526c8
Shir07 <priscila.ohannecian7@gmail.com>
2023-05-14 21:50:55 -0300

    deploy

--- src/main/java/com/portafoliobackend/portafoliobackend/controller/PersonaController.java
+++ src/main/java/com/portafoliobackend/portafoliobackend/controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "hhttps://portafolioap-f5519.web.app")
 public class PersonaController {
     @Autowired IPersonaServices ipersonaServices;
     
